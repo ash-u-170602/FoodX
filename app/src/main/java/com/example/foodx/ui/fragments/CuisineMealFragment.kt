d92e07519f97c8d7dd8c64d14b5d3ee7f441c532
Ashad Naushad <mohda170602@gmail.com>
2023-08-12 23:00:43 +0530

    bug fixed

--- app/src/main/java/com/example/foodx/ui/fragments/CuisineMealFragment.kt
+++ app/src/main/java/com/example/foodx/ui/fragments/CuisineMealFragment.kt
@@ -1,5 +1,6 @@
 package com.example.foodx.ui.fragments
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -33,6 +34,7 @@ class CuisineMealFragment : BaseFragment() {
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as MainActivity).viewModel
